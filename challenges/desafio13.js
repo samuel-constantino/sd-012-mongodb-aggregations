5ba2660a0b5707d7974336b2cd1e434a6e8d4fef
Samuel Constantino <samuelicapuidados@gmail.com>
2021-10-19 20:53:56 -0300

    desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,30 @@
+db.trips.aggregate([
+  {
+    $match: {
+      startTime:
+          {
+            $gte: ISODate("2016-03-10T00:00:00"),
+            $lte: ISODate("2016-03-10T23:59:59"),
+          },
+
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      duracao: {
+        $avg: {
+          $divide: [{
+            $subtract: ["$stopTime", "$startTime"],
+          }, 60000],
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: false,
+      duracaoMediaEmMinutos: { $ceil: ["$duracao"] },
+    },
+  },
+]);
